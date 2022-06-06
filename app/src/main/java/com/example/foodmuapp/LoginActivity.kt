ad3df8343d9b34fba514a953745224e9ff29d671
ahmaddzakki <ahmaddzakki9@gmail.com>
2022-06-06 13:39:29 +0700

    anjing

---
+++ app/src/main/java/com/example/foodmuapp/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.example.foodmuapp
+
+class LoginActivity {
+}
---
+++ app/src/main/java/com/example/foodmuapp/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.example.foodmuapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed( {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+}
